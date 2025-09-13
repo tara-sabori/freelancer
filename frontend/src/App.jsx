767872c640a22d87cf77ef8bbc934289ec36238a
tara-sabori <tarasaboori@gmail.com>
2025-09-14 00:06:22 +0330

    install react-hot-toast

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
+import { Route, Routes } from "react-router";
+import { Toaster } from "react-hot-toast";
+import AuthPage from "./pages/auth/AuthPage";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
-
+  const queryClient = new QueryClient();
   return (
-    <>
-      <p className='bg-primary-900 text-secondary-600 text-3xl'>سلام</p>
-    </>
-  )
+    <QueryClientProvider client={queryClient}>
+      <Toaster />
+      <Routes>
+        <Route path="/auth" element={<AuthPage />}></Route>
+      </Routes>
+    </QueryClientProvider>
+  );
 }
 
-export default App
+export default App;
