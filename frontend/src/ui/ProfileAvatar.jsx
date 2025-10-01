7fcfac7e370f6967db037e2b1fe6df4865f1454b
tara-sabori <tarasaboori@gmail.com>
2025-10-01 19:36:33 +0330

    add profile avatar

---
+++ frontend/src/ui/ProfileAvatar.jsx
@@ -0,0 +1,16 @@
+const ProfileAvatar = ({ avatarUrl, name }) => {
+  return (
+    <div className="flex items-center gap-2">
+      <div>
+        <img
+          src={avatarUrl || "/assets/images/user.jpg"}
+          className="w-8 h-8 rounded-full object-cover object-center"
+          alt="avatar"
+        />
+      </div>
+      <span className="text-sm text-secondary-600">{name}</span>
+    </div>
+  );
+};
+
+export default ProfileAvatar;
