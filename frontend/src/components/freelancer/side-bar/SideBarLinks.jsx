09a336915ce3a7db98b066b75a8a0994ee975f23
tara-sabori <tarasaboori@gmail.com>
2025-10-04 18:25:51 +0330

    change onclose handle

--- frontend/src/components/freelancer/side-bar/SideBarLinks.jsx
+++ frontend/src/components/freelancer/side-bar/SideBarLinks.jsx
@@ -1,8 +1,14 @@
+import { useEffect, useState } from "react";
 import { PiCardsThreeFill, PiHouseDuotone, PiStackBold } from "react-icons/pi";
 import { Link, useLocation } from "react-router";
 
 const SideBarLinks = ({ onClose }) => {
   const pathName = useLocation().pathname;
+  const [prevPath, setPrevPath] = useState(pathName);
+  useEffect(() => {
+    if (prevPath !== pathName) onClose();
+  }, [prevPath, pathName]);
+
   const links = [
     {
       title: "داشبورد",
@@ -20,7 +26,7 @@ const SideBarLinks = ({ onClose }) => {
     <ul className="flex flex-col gap-2 p-2">
       {links?.map((link, index) => (
         <li key={index}>
-          <Link to={link?.path} onClick={onClose}>
+          <Link to={link?.path}>
             <div
               className={`flex items-center gap-4 p-1.5 rounded-md text-sm ${link?.path === pathName ? "text-primary-900 bg-primary-50" : "text-secondary-800"}`}
             >
--- frontend/src/components/owner/side-bar/SideBarLinks.jsx
+++ frontend/src/components/owner/side-bar/SideBarLinks.jsx
@@ -1,8 +1,14 @@
+import { useEffect, useState } from "react";
 import { PiHouseDuotone, PiStackBold } from "react-icons/pi";
 import { Link, useLocation } from "react-router";
 
 const SideBarLinks = ({ onClose }) => {
   const pathName = useLocation().pathname;
+  const [prevPath, setPrevPath] = useState(pathName);
+  useEffect(() => {
+    if (prevPath !== pathName) onClose();
+  }, [prevPath, pathName]);
+
   const links = [
     { title: "داشبورد", path: "/owner/dashboard", icon: <PiHouseDuotone /> },
     { title: "پروژه‌ها", path: "/owner/projects", icon: <PiStackBold /> },
@@ -11,7 +17,7 @@ const SideBarLinks = ({ onClose }) => {
     <ul className="flex flex-col gap-2 p-2">
       {links?.map((link, index) => (
         <li key={index}>
-          <Link to={link?.path} onClick={onClose}>
+          <Link to={link?.path}>
             <div
               className={`flex items-center gap-4 p-1.5 rounded-md text-sm ${link?.path === pathName ? "text-primary-900 bg-primary-50" : "text-secondary-800"}`}
             >
