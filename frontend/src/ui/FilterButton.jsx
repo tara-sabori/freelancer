51a00d2fdc86471a9e88f619ae683a49943b1773
tara-sabori <tarasaboori@gmail.com>
2025-10-04 18:26:11 +0330

    add filter dropdown and filter button

---
+++ frontend/src/ui/FilterButton.jsx
@@ -0,0 +1,28 @@
+import { useSearchParams } from "react-router";
+
+const FilterButton = ({ filterField, title, data }) => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const value = searchParams.get(filterField) || "ALL";
+  const handleClick = (item) => {
+    searchParams.set(filterField, item);
+    setSearchParams(searchParams);
+  };
+  return (
+    <div className="flex items-center gap-1.5">
+      <span className="text-xs">{title}</span>
+      <div className="flex items-center w-fit overflow-hidden bg-secondary-50 rounded-md border border-secondary-400">
+        {data?.map((d) => (
+          <button
+            className={`text-xs py-2 px-3 cursor-pointer ${value === d?.value ? "bg-primary-900 text-white font-semibold" : "bg-secondary-50 text-secondary-700"}`}
+            key={d?.value}
+            onClick={() => handleClick(d?.value)}
+          >
+            {d?.label}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default FilterButton;
---
+++ frontend/src/ui/FilterDropDown.jsx
@@ -0,0 +1,32 @@
+import { useSearchParams } from "react-router";
+
+const FilterDropDown = ({ filterField, options, isLoading = false }) => {
+  const [searchParams, setSearchParams] = useSearchParams() || "ALL";
+  const value = searchParams.get(filterField);
+  const handleChange = (e) => {
+    searchParams.set(filterField, e.target.value);
+    setSearchParams(searchParams);
+  };
+  return (
+    <div>
+      {isLoading ? (
+        <div className="w-[135px] rounded-md h-9 bg-secondary-300 animate-pulse"></div>
+      ) : (
+        <select
+          value={value}
+          onChange={handleChange}
+          className="border border-secondary-400 p-1.5 w-[135px] text-xs rounded-md outline-none focus:shadow-sm bg-secondary-50 cursor-pointer"
+        >
+          {options?.length > 0 &&
+            options.map((option) => (
+              <option key={option.value} value={option.value}>
+                {option.label}
+              </option>
+            ))}
+        </select>
+      )}
+    </div>
+  );
+};
+
+export default FilterDropDown;
