2c34738d02544de47026991fb901c95319d65367
tara-sabori <tarasaboori@gmail.com>
2025-10-06 20:48:21 +0330

    add loading component

---
+++ frontend/src/ui/Loading.jsx
@@ -0,0 +1,11 @@
+import { HashLoader } from "react-spinners";
+
+const Loading = () => {
+  return (
+    <div className="w-full h-[80vh] flex items-center justify-center">
+      <HashLoader color="#4a6dff" />
+    </div>
+  );
+};
+
+export default Loading;
