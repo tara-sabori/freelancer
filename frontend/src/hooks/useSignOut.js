311f0a72b7edb3e6cc4448fea79cc4b6cf800ea5
tara-sabori <tarasaboori@gmail.com>
2025-10-10 01:06:35 +0330

    add removeQueries to useSignout

--- frontend/src/hooks/useSignOut.js
+++ frontend/src/hooks/useSignOut.js
@@ -1,11 +1,18 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useNavigate } from "react-router";
 import { signOutApi } from "../services/AuthServices";
 import toast from "react-hot-toast";
 
 export default function useSignOut() {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+
   const { isPending, mutate: signOutFn } = useMutation({
     mutationFn: signOutApi,
-    onSuccess: () => toast.success("از حساب  کاربری خود خارج شدید"),
+    onSuccess: () => {
+      queryClient.removeQueries();
+      navigate("/", { replace: true });
+    },
     onError: (err) =>
       toast.error(err?.response?.data?.message || "مشکلی رخ داده است."),
   });
