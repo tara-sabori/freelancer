5745a1a3a3d9f9a8aecf75037f98733b5de1e107
tara-sabori <tarasaboori@gmail.com>
2025-10-12 22:35:21 +0330

    add move back to send otp form

--- frontend/src/components/authentication/SendOtpForm.jsx
+++ frontend/src/components/authentication/SendOtpForm.jsx
@@ -1,3 +1,5 @@
+import { PiCaretLeftLight } from "react-icons/pi";
+import useMoveBack from "../../hooks/useMoveBack";
 import SubmitButton from "../../ui/SubmitButton";
 
 const SendOtpForm = ({ onSubmit, isPending, setPhoneNumber, phoneNumber }) => {
@@ -5,9 +7,19 @@ const SendOtpForm = ({ onSubmit, isPending, setPhoneNumber, phoneNumber }) => {
     e?.preventDefault();
     onSubmit();
   };
+  const moveBack = useMoveBack();
   return (
-    <div>
-      <form className="flex flex-col gap-4" onSubmit={submitHandle}>
+    <div className="space-y-4">
+      <div className="flex justify-end">
+        <button
+          className="flex items-center gap-1 text-secondary-800 cursor-pointer"
+          onClick={moveBack}
+        >
+          <span className="text-sm">بازگشت</span>
+          <PiCaretLeftLight />
+        </button>
+      </div>
+      <form className="flex flex-col gap-4 px-4" onSubmit={submitHandle}>
         <div className="flex flex-col gap-4">
           <label htmlFor="t1" className="text-secondary-800 text-sm">
             شماره موبایل
